6afce84151445ec40c3a159c2d81e73c43f1626c
JosianDevPython <josianhenrique.devpython@gmail.com>
2024-10-29 20:11:49 -0300

    strings

---
+++ name.py
@@ -0,0 +1,6 @@
+# string com letras minusculas é armazenada na variavel name
+# o método é uma açao nesse caso, title aparece depois da varial name informando o (.) 
+# depois de name informa que title deve atuar na variavel name, todo método vem acompanhado de parenteses
+# title() é um metodo para deixar letras minúsculas
+name = "ada lovelace" 
+print(name.title()) 
