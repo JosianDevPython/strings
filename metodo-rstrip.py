f82769150279d400b84c059d9c9fb2447d8c4c59
JosianDevPython <josianhenrique.devpython@gmail.com>
2024-10-31 12:37:15 -0300

    metodo strip

---
+++ metodo-rstrip.py
@@ -0,0 +1,8 @@
+favorite_linguage = 'python    '
+favorite_linguage = favorite_linguage.rstrip() # remove espaços da direita de forma permanente
+
+favorite_linguage = '  python  ' 
+favorite_linguage.rstrip() # remove espaços da direita da string
+favorite_linguage.lstrip() # # remove espaços da da esquerda da string
+favorite_linguage.strip()  #remove espaços dos dois lados da string
+
